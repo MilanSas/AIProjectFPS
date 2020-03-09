bb152b37c31a3b78de31ef50b3df94345e0e6289
MilanSas <milansas1@gmail.com>
2020-03-09 14:18:00 +0100

    Beginning of making object aim controller

--- Assets/ObjectControllerScript.cs
+++ Assets/ObjectControllerScript.cs
@@ -6,13 +6,26 @@ public class ObjectControllerScript : MonoBehaviour
 {
     public TargetHitboxScript target;
     public PlayerCharacterController player;
-
-    public float xAxis = 0;
-    public float yAxis = 90; 
+    public PlayerWeaponsManager playerWeapon;
+    public Vector3 playerPosition;
+    public Vector3 targetPosition;
+    private WeaponController activeWeapon;
+    private float _nextRelease = 0.0f;
+    private float _nextCharge = 0.0f;
+    public float chargePercentage = 0.5f;
+    private float _chargeTime;
+    private float _shotDelay = 1.0f;
+    private bool _isCharging = false;
+    private bool _hasFired;
+    public float xAxis = 5;
+    public float yAxis = -45; 
     // Start is called before the first frame update
     void Start()
     {
+        activeWeapon = playerWeapon.GetActiveWeapon();
+        Debug.Log(activeWeapon.maxChargeDuration);
         
+        _nextCharge = 1.0f;
     }
 
     // Update is called once per frame
@@ -20,5 +33,24 @@ public class ObjectControllerScript : MonoBehaviour
     {
         player.setXLookAxis(xAxis);
         player.setYLookAxis(yAxis);
+
+        _chargeTime = activeWeapon.maxChargeDuration * chargePercentage;
+
+        Debug.Log("Time " + Time.time + " NextCharge " + _nextCharge);
+        if (!_isCharging && Time.time > _nextCharge)
+        {
+            _hasFired = activeWeapon.HandleShootInputs(false, true, false);
+            _nextRelease = Time.time + _chargeTime;
+            _isCharging = true;
+        }
+
+        else if (_isCharging && !_hasFired && Time.time > _nextRelease)
+        {
+            _hasFired = activeWeapon.HandleShootInputs(false, false, true);
+            _nextRelease = Time.time + _chargeTime;
+            _nextCharge = Time.time + _shotDelay;
+            _isCharging = false;
+        }
+        
     }
 }
